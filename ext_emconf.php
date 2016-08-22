6819ad04260745e3687efb4308b08db20e91fb52
Florian Wessels <f.wessels@bitmotion.de>
2016-08-22 15:46:27 +0200

    [TASK] Add TYPO3 6.2 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6',
+            'typo3' => '6.2.0-7.6.99',
             'php' => '5.5',
         ],
         'conflicts' => [],
