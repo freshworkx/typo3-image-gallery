6dea76d85470eea02451790790678bdbd806df1a
Florian Wessels <f.wessels@bitmotion.de>
2019-04-03 10:05:29 +0200

    [TASK] Set version to 4.0.1-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Simple Image Gallery',
     'description' => 'Simple gallery using file collections.',
-    'version' => '4.0.0',
+    'version' => '4.0.1-dev',
     'category' => 'plugin',
     'author' => 'Bitmotion GmbH',
     'author_email' => 'typo3-ext@bitmotion.de',
