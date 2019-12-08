9502d0662a11bc9659bdbc166efb4842128ab4ca
Florian Wessels <f.wessels@bitmotion.de>
2019-12-08 11:44:57 +0100

    [TASK] Call user functions in configuration files

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,15 +1,24 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Bitmotion.bm_image_gallery',
-    'List',
-    [
-        'List' => 'list,gallery,selectedGallery',
-    ],
-    []
+call_user_func(
+    function ($extensionKey) {
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'Bitmotion.bm_image_gallery',
+            'List',
+            [
+                'List' => 'list,gallery,selectedGallery',
+            ],
+            []
+        );
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+            sprintf(
+                '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:%s/Configuration/TSconfig/ContentElementWizard.tsconfig">',
+                $extensionKey
+            )
+        );
+    }, 'bm_image_gallery'
 );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:bm_image_gallery/Configuration/TSconfig/ContentElementWizard.tsconfig">'
-);
+
