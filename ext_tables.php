99d8c0a2e2467c0e736b61509e641743867a6b35
Florian Wessels <f.wessels@bitmotion.de>
2019-12-09 09:55:12 +0100

    [TASK] Apply CS

--- ext_tables.php
+++ ext_tables.php
@@ -1,7 +1,6 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-
 call_user_func(
     function ($extensionKey) {
         $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
