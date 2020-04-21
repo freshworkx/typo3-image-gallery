0e312b8abb8956d6642ea62c5471c66c54c9dc7c
Florian Wessels <f.wessels@bitmotion.de>
2020-04-21 21:34:54 +0200

    [TASK] Drop TYPO3 v8 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF['bm_image_gallery'] = [
     'clearCacheOnLoad' => false,
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-10.3.99',
+            'typo3' => '9.5.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
