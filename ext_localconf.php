6b324ef4fc5554e3529a94f60d2dd645ca8b1bdf
Florian Wessels <flo.wessels@gmail.com>
2020-06-02 15:50:47 +0200

    [TASK] Update plugin registration

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,12 +3,20 @@ defined('TYPO3_MODE') || die('Access denied.');
 
 call_user_func(
     function ($extensionKey) {
+        // TODO: Remove this condition when dropping TYPO3 v9 support.
+        if (version_compare(TYPO3_version, '10.0.0', '>=')) {
+            $extensionName = 'BmImageGallery';
+            $controllerName = \Bitmotion\BmImageGallery\Controller\ListController::class;
+        } else {
+            $extensionName = 'Bitmotion.BmImageGallery';
+            $controllerName = 'List';
+        }
+
+        // Configure Auth0 plugin
         \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-            'Bitmotion.' . $extensionKey,
+            $extensionName,
             'List',
-            [
-                'List' => 'list,gallery,selectedGallery',
-            ],
+            [$controllerName => 'list,gallery,selectedGallery'],
             []
         );
     }, 'bm_image_gallery'
