7a1da9abfeaa3b5f19fcedd4a6ff71f640e5d70b
Florian Wessels <flo.wessels@gmail.com>
2020-06-17 17:13:39 +0200

    [TASK] Use constants

--- Classes/Domain/Repository/FileCollectionRepository.php
+++ Classes/Domain/Repository/FileCollectionRepository.php
@@ -27,11 +27,13 @@ use TYPO3\CMS\Frontend\Resource\FileCollector;
 
 class FileCollectionRepository extends Typo3FileCollectionRepository
 {
-    const ORDER_BY_DEFAULT = 'default';
+    const SORTING_PROPERTY_DEFAULT = 'default';
 
     const SORTING_ORDER_ASC = 'ascending';
     const SORTING_ORDER_DESC = 'descending';
     const SORTING_ORDER_RAND = 'random';
+
+    const TABLE_NAME = 'sys_file_collection';
 
 
     protected $languageUid;
@@ -43,8 +45,8 @@ class FileCollectionRepository extends Typo3FileCollectionRepository
     public function __construct(Context $context)
     {
         $this->languageUid = $context->getPropertyFromAspect('language', 'id');
-        $this->languageField = $GLOBALS['TCA']['sys_file_collection']['ctrl']['languageField'];
-        $this->languagePointer = $GLOBALS['TCA']['sys_file_collection']['ctrl']['transOrigPointerField'];
+        $this->languageField = $GLOBALS['TCA'][self::TABLE_NAME]['ctrl']['languageField'];
+        $this->languagePointer = $GLOBALS['TCA'][self::TABLE_NAME]['ctrl']['transOrigPointerField'];
     }
 
     /**
@@ -85,22 +87,23 @@ class FileCollectionRepository extends Typo3FileCollectionRepository
     /**
      * @throws ResourceDoesNotExistException
      */
-    public function getFileCollectionById(string $identifier, string $orderBy = '', int $maxItems = 0, ?string $sortingOrder = null): array
+    public function getFileCollectionById(string $identifier, string $sortingProperty = self::SORTING_PROPERTY_DEFAULT, int $maxItems = 0, string $sortingOrder = self::SORTING_ORDER_ASC): array
     {
         $fileCollections = $this->getFileCollectionsToDisplay($identifier);
         $fileCollector = GeneralUtility::makeInstance(FileCollector::class);
         $fileCollector->addFilesFromFileCollections($fileCollections);
 
-        if ($orderBy === '' || $orderBy !== self::ORDER_BY_DEFAULT) {
-            $fileCollector->sort($orderBy, $sortingOrder ?? self::SORTING_ORDER_ASC);
+        if ($sortingProperty !== self::SORTING_PROPERTY_DEFAULT) {
+            $fileCollector->sort($sortingProperty, $sortingOrder);
         }
 
         $fileObjects = GeneralUtility::makeInstance(FileFactory::class)->getFileObjects($fileCollector->getFiles(), $maxItems);
 
-        if (count($fileObjects) > 0) {
-            $fileCollectionUid = array_shift($fileCollections);
-            $fileCollection = $this->findByUid($fileCollectionUid);
-            $collectionInfo = new CollectionInfo($fileCollection, $fileObjects);
+        if (!empty($fileObjects)) {
+            $collectionInfo = new CollectionInfo(
+                $this->findByUid(array_shift($fileCollections)),
+                $fileObjects
+            );
         }
 
         return [
@@ -111,14 +114,14 @@ class FileCollectionRepository extends Typo3FileCollectionRepository
 
     protected function getLocalizedFileCollection(int &$fileCollectionUid)
     {
-        $fileCollection = BackendUtility::getRecord('sys_file_collection', $fileCollectionUid);
+        $fileCollection = BackendUtility::getRecord(self::TABLE_NAME, $fileCollectionUid);
 
         if ($this->languageUid !== (int)$fileCollection[$this->languageField]) {
-            $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('sys_file_collection');
+            $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable(self::TABLE_NAME);
 
             $localizedFileCollection = $queryBuilder
                 ->select('uid')
-                ->from('sys_file_collection')
+                ->from(self::TABLE_NAME)
                 ->where($queryBuilder->expr()->eq($this->languageField, $queryBuilder->createNamedParameter($this->languageUid, \PDO::PARAM_INT)))
                 ->andWhere($queryBuilder->expr()->eq($this->languagePointer, $queryBuilder->createNamedParameter($fileCollectionUid, \PDO::PARAM_INT)))
                 ->execute()
