cd99d14f4aa97284fb6085d1f3e7da613f031bde
Florian Wessels <flo.wessels@gmail.com>
2020-06-17 16:54:58 +0200

    [TASK] Add ClassAliasMap

---
+++ Migrations/Code/ClassAliasMap.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    '\Bitmotion\BmImageGallery\Controller\ListController' => \Leuchtfeuer\BmImageGallery\Controller\ListController::class,
+    '\Bitmotion\BmImageGallery\Domain\Repository\FileCollectionRepository' => \Leuchtfeuer\BmImageGallery\Domain\Repository\FileCollectionRepository::class,
+    '\Bitmotion\BmImageGallery\Domain\Transfer\CollectionInfo' => \Leuchtfeuer\BmImageGallery\Domain\Transfer\CollectionInfo::class,
+    '\Bitmotion\BmImageGallery\Factory\FileFactory' => \Leuchtfeuer\BmImageGallery\Factory\FileFactory::class,
+];
---
+++ Migrations/Code/LegacyClassesForIde.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace {
+    die('Access denied');
+}
+
+namespace Bitmotion\BmImageGallery\Cache {
+
+    abstract class ListController extends \Leuchtfeuer\BmImageGallery\Controller\ListController
+    {
+    }
+}
+
+namespace Bitmotion\BmImageGallery\Domain\Repository {
+
+    class FileCollectionRepository extends \Leuchtfeuer\BmImageGallery\Domain\Repository\FileCollectionRepository
+    {
+    }
+}
+
+namespace Bitmotion\BmImageGallery\Domain\Transfer {
+
+    class CollectionInfo extends \Leuchtfeuer\BmImageGallery\Domain\Transfer\CollectionInfo
+    {
+    }
+}
+
+namespace Bitmotion\BmImageGallery\Factory {
+
+    class FileFactory extends \Leuchtfeuer\BmImageGallery\Factory\FileFactory
+    {
+    }
+}
