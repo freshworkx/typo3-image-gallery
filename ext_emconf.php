b415068ddce8ec45fb9d29556dd21cc716917e16
Florian Wessels <flo.wessels@gmail.com>
2020-08-10 15:02:59 +0200

    [TASK] Raise dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF['bm_image_gallery'] = [
     'clearCacheOnLoad' => false,
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.4.99',
+            'typo3' => '10.4.6-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
