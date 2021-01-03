9f9dad8bb2c2df0d4f78cf112b07f1f4440a203a
Florian Wessels <flo.wessels@gmail.com>
2021-01-03 16:56:51 +0100

    [TASK] Kickstart functional tests

---
+++ Tests/Functional/Factory/FileFactoryTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace Leuchtfeuer\BmImageGallery\Tests\Functional\Factory;
+
+use Leuchtfeuer\BmImageGallery\Factory\FileFactory;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+
+/**
+ * @covers \Leuchtfeuer\BmImageGallery\Factory\FileFactory
+ */
+class FileFactoryTest extends FunctionalTestCase
+{
+    protected $subject;
+
+    protected $testExtensionsToLoad = [
+        'typo3conf/ext/bm_image_gallery'
+    ];
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->subject = GeneralUtility::makeInstance(FileFactory::class);
+    }
+}
