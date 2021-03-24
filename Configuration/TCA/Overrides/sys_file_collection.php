7fdf40c5a312aaae00af850d9e52737899db15bc
Florian Wessels <flo.wessels@gmail.com>
2021-03-24 08:48:21 +0100

    [FEATURE] Extend gallery data by datetime and location
    Implements #16

--- Configuration/TCA/Overrides/sys_file_collection.php
+++ Configuration/TCA/Overrides/sys_file_collection.php
@@ -3,13 +3,6 @@ declare(strict_types = 1);
 defined('TYPO3_MODE') || die('Access denied.');
 
 $temporaryColumns = [
-    'bm_image_gallery_description' => [
-        'label' => 'LLL:EXT:bm_image_gallery/Resources/Private/Language/locallang_be.xlf:bm_image_gallery_description',
-        'config' => [
-            'type' => 'text',
-            'enableRichtext' => true,
-        ],
-    ],
     'bm_image_gallery_path_segment' => [
         'label' => 'LLL:EXT:bm_image_gallery/Resources/Private/Language/locallang_be.xlf:bm_image_gallery_path_segment',
         'config' => [
@@ -26,13 +19,35 @@ $temporaryColumns = [
             'eval' => 'uniqueInPid',
         ],
     ],
+    'bm_image_gallery_location' => [
+        'label' => 'LLL:EXT:bm_image_gallery/Resources/Private/Language/locallang_be.xlf:bm_image_gallery_location',
+        'config' => [
+            'type' => 'input',
+            'size' => 30,
+            'eval' => 'trim',
+        ],
+    ],
+    'bm_image_gallery_date' => [
+        'label' => 'LLL:EXT:bm_image_gallery/Resources/Private/Language/locallang_be.xlf:bm_image_gallery_date',
+        'config' => [
+            'default' => 0,
+            'type' => 'input',
+            'renderType' => 'inputDateTime',
+            'eval' => 'datetime,int',
+        ],
+    ],
+    'bm_image_gallery_description' => [
+        'label' => 'LLL:EXT:bm_image_gallery/Resources/Private/Language/locallang_be.xlf:bm_image_gallery_description',
+        'config' => [
+            'type' => 'text',
+            'enableRichtext' => true,
+        ],
+    ],
 ];
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('sys_file_collection', $temporaryColumns);
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
     'sys_file_collection',
-    implode(',', array_keys($temporaryColumns)),
-    '',
-    'after:title'
+    '--div--;LLL:EXT:bm_image_gallery/Resources/Private/Language/locallang_be.xlf:bm_image_gallery_tab,' . implode(',', array_keys($temporaryColumns)),
 );
