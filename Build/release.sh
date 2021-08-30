f18d4fc89abc3899b268610a0fd52f4dd57e1ba7
Max Rösch <github@elbebass.com>
2021-08-30 11:51:19 +0200

    [TASK] Add release script for TER releases

---
+++ Build/release.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+NEW_RELEASE=$1
+LATEST_RELEASE=$(git tag --sort=committerdate --list | tail -1)
+DIR=$(pwd)
+
+
+if [ ! -z "${NEW_RELEASE}" ]; then
+  NEW_RELEASE=v${NEW_RELEASE}
+  echo "Create release of version ${NEW_RELEASE}"
+else
+  echo "No version defined. Exit."
+  exit 1;
+fi
+
+EXISTS=$(git describe --contains ${NEW_RELEASE} 2>&1)
+
+if [[ "$EXISTS" == "$NEW_RELEASE" ]]; then
+  echo "Release already exists. Exit."
+  exit 1;
+fi
+
+if [ -d "$DIR" ]; then
+
+  echo "Create git tag"
+  cd $DIR
+  git tag $NEW_RELEASE
+
+  echo "Archive repository..."
+  zip -r "../bm_image_gallery_${1}.zip" * -x \*.git\* Build/\* Documentation/\* Tests/\* php-cs-fixer.php codecov.yml LICENSE.txt README.md
+  echo "Done."
+
+  echo "Please add and push the git tag: gp --tags"
+else
+  echo "This script has to be executed from the git root directory!"
+  exit 1;
+fi
+
+exit 0;
