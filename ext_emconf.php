88d9478abe9f33783e21eb4f8bc11a2b92de4069
Jens Neumann <info@freshworkx.de>
2024-06-07 10:57:36 +0200

    [RELEASE] Release of version 6.2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 $EM_CONF['bm_image_gallery'] = [
     'title' => 'Simple Image Gallery',
     'description' => 'Simple gallery using file collections.',
-    'version' => '6.1.0',
+    'version' => '6.2.0',
     'category' => 'plugin',
     'author' => 'Jens Neumann',
     'author_email' => 'info@freshworkx.de',
