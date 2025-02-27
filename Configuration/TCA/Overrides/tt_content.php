b5a932f366ad7179c7691ea892dc3a677b2ba505
Jens Neumann <info@freshworkx.de>
2025-02-27 15:26:27 +0100

    [!!!][REFACTOR] refactor and modernize code basis
    - update plugin configuration in ext_localconf.php
    - update plugin registration in tt_content.php
    - remove page.tsconfig
    - remove file_collection field from flexForm, use field from tt_content due to the correct translation behavior
    - update, consolidate and streamline the flexForms
    - rename ListController.php to GalleryController.php
    - completely rewritten GalleryController.php
    - update templates to work with GalleryController again
    - remove duplication of the description field in partial Gallery.html
    - update language files

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -5,35 +5,25 @@ declare(strict_types=1);
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
-defined('TYPO3') || die('Access denied.');
+defined('TYPO3') || die();
 
-call_user_func(
-    function ($extensionKey) {
-        $plugins = [
-            'GalleryList' => 'display_mode.1',
-            'GalleryDetail' => 'display_mode.2',
-            'SelectedGallery' => 'display_mode.3'
-        ];
-        foreach ($plugins as $pluginName => $label) {
-            $pluginSignature = ExtensionUtility::registerPlugin(
-                $extensionKey,
-                $pluginName,
-                'LLL:EXT:bm_image_gallery/Resources/Private/Language/locallang_be.xlf:ffds.' . $label,
-                'bm-image-gallery',
-                'LLL:EXT:bm_image_gallery/Resources/Private/Language/locallang_be.xlf:bm_image_gallery_tab'
-            );
-            ExtensionManagementUtility::addToAllTCAtypes(
-                'tt_content',
-                '--div--;Plugin,pi_flexform,',
-                $pluginSignature,
-                'after:subheader',
-            );
-            ExtensionManagementUtility::addPiFlexFormValue(
-                '*',
-                'FILE:EXT:bm_image_gallery/Configuration/FlexForms/' . $pluginName . '.xml',
-                $pluginSignature
-            );
-        }
-    },
-    'bm_image_gallery'
-);
+$plugins = ['GalleryList', 'GalleryDetail', 'SelectedGallery'];
+foreach ($plugins as $pluginName) {
+    $pluginSignature = ExtensionUtility::registerPlugin(
+        'BmImageGallery',
+        $pluginName,
+        'LLL:EXT:bm_image_gallery/Resources/Private/Language/locallang_be.xlf:plugin.' . $pluginName,
+        'bm-image-gallery',
+    );
+    ExtensionManagementUtility::addToAllTCAtypes(
+        'tt_content',
+        '--div--;Plugin,file_collections,pi_flexform,',
+        $pluginSignature,
+        'after:subheader',
+    );
+    ExtensionManagementUtility::addPiFlexFormValue(
+        '*',
+        'FILE:EXT:bm_image_gallery/Configuration/FlexForms/PluginSettings.xml',
+        $pluginSignature
+    );
+}
