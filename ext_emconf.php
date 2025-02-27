1da8516af3ce2d76975b2822fb6039c0e50d116c
Jens Neumann <info@freshworkx.de>
2025-02-27 15:54:38 +0100

    [TASK] update composer.json and ext_emconf.php for correct constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,14 +2,14 @@
 $EM_CONF['bm_image_gallery'] = [
     'title' => 'Simple Image Gallery',
     'description' => 'Simple gallery using file collections.',
-    'version' => '6.2.2',
+    'version' => '7.0.0',
     'category' => 'plugin',
     'author' => 'Jens Neumann',
     'author_email' => 'info@freshworkx.de',
     'state' => 'stable',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.11-13.4.99',
+            'typo3' => '13.4.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
