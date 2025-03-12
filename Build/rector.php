6cf6da634d70748a285814891d13bdc19876e35d
Jens Neumann <info@freshworkx.de>
2025-03-12 17:56:36 +0100

    [FEATURE] introduce new flexForm migration wizard
    - migrate flexForm of existing plugins to new structure
    - including unit tests

--- Build/rector.php
+++ Build/rector.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\ValueObject\PhpVersion;
 use Ssch\TYPO3Rector\Set\Typo3LevelSetList;
@@ -12,12 +13,15 @@ return RectorConfig::configure()
         __DIR__ . '/../Build',
         __DIR__ . '/../Classes',
         __DIR__ . '/../Configuration',
+        __DIR__ . '/../Tests',
     ])
     ->withPhpSets(false, true)
     ->withPreparedSets(true, true, true)
     ->withSets([
         LevelSetList::UP_TO_PHP_84,
         Typo3LevelSetList::UP_TO_TYPO3_13,
+        PHPUnitSetList::PHPUNIT_110
+
     ])
     ->withPhpVersion(PhpVersion::PHP_82)
 ;
