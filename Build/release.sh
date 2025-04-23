f491dd368bfc1e3f69efbf0cf9df790399da36bd
Jens Neumann <info@freshworkx.de>
2025-04-23 16:27:48 +0200

    [BUGFIX] prevent to include optionally available composer.lock file

--- Build/release.sh
+++ Build/release.sh
@@ -25,7 +25,7 @@ if [ -d "$DIR" ]; then
   git tag "$NEW_RELEASE"
 
   echo "Archive repository..."
-  zip -r "../bm_image_gallery_${1}.zip" ./* -x \*.git\* Build/ Tests/\*
+  zip -r "../bm_image_gallery_${1}.zip" ./* -x \*.git\* Build/ Tests/ composer.lock\*
   echo "Done."
 
   echo "Please add and push the git tag: gp --tags"
