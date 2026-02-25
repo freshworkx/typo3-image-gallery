4df05fbaf40d5abec7a0dc67bf9d160c67842a45
Jens Neumann <info@freshworkx.de>
2026-02-25 14:00:46 +0100

    feat(v14): prepare v14 compatibility
    - Update TYPO3 core dependencies to v14
    - Modify TYPO3 version constraints in ext_emconf.php
    - Upgrade dev-dependencies to latest compatible versions
    
    BREAKING CHANGE: drop TYPO3 core v13 support
    Refs: #76

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,7 @@ $EM_CONF['bm_image_gallery'] = [
     'state' => 'stable',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.4.0-13.4.99',
+            'typo3' => '14.4.0-14.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
